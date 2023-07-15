ca5d64b1f98c923b060c9084734fd5f322609c40
PivovarovIV <brauer.igor@gmail.com>
2023-07-16 02:26:25 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+Мок http-server
+http://{host name}:8000/test/cache - тестирование кэш запросов на http-клиенте
---
+++ src/main/kotlin/HttpMockServer.kt
@@ -0,0 +1,52 @@
+import com.sun.net.httpserver.HttpServer
+import com.sun.net.httpserver.spi.HttpServerProvider
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import java.io.OutputStream
+import java.net.InetSocketAddress
+import java.net.Socket
+
+fun main() {
+    printLocalIpAddress()
+    val httpServerProvider: HttpServerProvider = HttpServerProvider.provider()
+
+    val socketAddress = InetSocketAddress(8000)
+
+    val httpServer: HttpServer = httpServerProvider.createHttpServer(socketAddress, 0)
+    httpServer.createContext("/test/cache") { httpExchange ->
+        val remoteAddress = httpExchange.remoteAddress
+        val now = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+        println("$now ${httpExchange.requestMethod} from $remoteAddress")
+        val response = StringBuilder()
+            .appendLine(now)
+            .append("protocol: ").appendLine(httpExchange.protocol)
+            .append("request URI: ").appendLine(httpExchange.requestURI)
+            .append("method: ").appendLine(httpExchange.requestMethod)
+            .append("local address: ").appendLine(httpExchange.localAddress)
+            .append("remote address: ").appendLine(remoteAddress)
+            .append("headers:").appendLine()
+        httpExchange.requestHeaders.forEach {
+            response.append('\t').append(it.key).append(" ").appendLine(it.value)
+        }
+        val requestBody = httpExchange.requestBody.use {
+            it.readAllBytes().decodeToString()
+        }
+        response.appendLine()
+            .appendLine("request body:")
+            .appendLine(requestBody)
+        httpExchange.responseHeaders["Cache-Control"] = listOf("max-age=60")
+        httpExchange.sendResponseHeaders(200, response.length.toLong())
+        val os: OutputStream = httpExchange.responseBody
+        os.write(response.toString().toByteArray())
+        os.close()
+    }
+    httpServer.start()
+    println("the http server is running")
+}
+
+private fun printLocalIpAddress() = Socket().use { socket ->
+    socket.connect(InetSocketAddress("ya.ru", 80))
+    val localIp = socket.localAddress.hostAddress
+    println("local IP address: $localIp")
+}
